2a08d3fc7e95b70f9361611bea2abbe7ddf53f46
justinro-underscore <jcroderman@gmail.com>
2021-09-16 09:36:24 -0700

    Adds popup for describing projects

--- JustinWebpage/Projects/projects.js
+++ JustinWebpage/Projects/projects.js
@@ -1,7 +1,26 @@
-function scrollToCode()
-{
-  document.getElementById("Code").scrollIntoView({
-    behavior: "smooth",
-    block: "start"
-  });
+function onLoad() {
+  if (window.location.hash !== '') {
+    const id = window.location.hash.slice(1);
+    openPopup(id);
+  }
+}
+
+function openPopup(popupId) {
+  if (document.getElementById(`popup-${popupId}`) !== null) {
+    document.getElementById("popup-background").classList.remove("popup-background-hidden");
+    document.getElementById(`popup-${popupId}`).style.display = '';
+  }
+}
+
+function closePopups() {
+  let popups = document.getElementsByClassName("popup");
+  for (let i = 0; i < popups.length; i++) {
+    popups.item(i).style.display = 'none';
+  }
+  document.getElementById("popup-background").classList.add("popup-background-hidden");
+}
+
+function dontClose(event) {
+  console.log(event);
+  event.stopPropogation();
 }
