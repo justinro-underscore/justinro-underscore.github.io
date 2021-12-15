e85611ee5ac7074118bebf15449b5d5a1347e478
justinro-underscore <jcroderman@gmail.com>
2021-12-14 21:30:12 -0800

    Adds static level description for Picross

--- Picross/scripts/constants.js
+++ Picross/scripts/constants.js
@@ -1,5 +1,11 @@
 // IDs
 const ID_LEVELS = 'levels';
+const ID_LEVEL_DESC_PREVIEW_CONTAINER = 'level-description-preview-container';
+const ID_LEVEL_DESC_PREVIEW = 'level-description-preview';
+const ID_LEVEL_DESC_TITLE = 'level-description-title';
+const ID_LEVEL_DESC_LOCATION = 'level-description-location';
+const ID_LEVEL_DESC_SIZE = 'level-description-size';
+const ID_LEVEL_DESC_TIME = 'level-description-time';
 
 // Classes
 const CLASS_LEVEL_CELL = 'level-cell';
